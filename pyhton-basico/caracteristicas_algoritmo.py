55f4c9d0807f67c548cb1ed419f89318d2000c69
jonlucaz <jonathanlucastri@gmail.com>
2022-09-30 10:00:33 -0300

    novos cursos

---
+++ pyhton-basico/caracteristicas_algoritmo.py
@@ -0,0 +1,15 @@
+#características de um algoritmo:
+#ponto inicial e ponto final
+#não ser ambíguo
+#tratar dados externos
+#etapas alcançáveis
+
+#composição de um algoritmo:
+#entrada
+#processamento
+#saída
+
+#formas de representação:
+#descrição narrativa
+#fluxograma
+#pseudocódigo
---
+++ pyhton-basico/variaveis.py
@@ -0,0 +1,5 @@
+code = 105
+
+
+print("Code: ", code) 
+print(type(code))
